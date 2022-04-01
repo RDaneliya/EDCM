2e209fd7ff056d0d3fa73d8376ca9b1874d61a4f
GlozZzy <kpackl14@gmail.com>
2022-04-01 22:05:33 +0700

    add info

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Ccomponent from "./Ccomponent";
 
 function App() {
+
   return (
     <div className="App">
+      <Ccomponent/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>sdfsd</code> and save to reload.
         </p>
         <a
           className="App-link"
