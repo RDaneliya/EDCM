685078131f2f3c4328bcc881b2ddce1c2bd4ddfd
GlozZzy <kpackl14@gmail.com>
2022-04-11 21:59:36 +0700

    add mainpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,21 +6,34 @@ function App() {
 
   return (
     <div className="App">
-      <Ccomponent/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>sdfsd</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div>Elite: Dangerous Commodity Monitoring</div>
+          <div>
+            <a href="#" className="right">API</a>
+            <a href="#" className="right">Bot</a>
+            <a href="#" className="right">Help</a>
+          </div>
       </header>
+
+        <div className="mainblock">
+            <block className="blue_block">
+                <h3>Stations</h3>
+                <p>Browse the universe! Jump to any stations or search
+                    by many properties. You can even find the ship you
+                    wanna buy!</p>
+            </block>
+            <block className="yellow_block">
+                <h3>Commodities</h3>
+                <p>Browse commodities, compare and sort by numbers and
+                    go into detail to find out who sells or buys the best!</p>
+            </block>
+            <block className="gray_block">
+                <h3>Find Commodity</h3>
+                <p>Mission problems? Can't find a station that sells you
+                    that stuff? Then the Commodity Finder is what you need!</p>
+            </block>
+        </div>
+
     </div>
   );
 }
